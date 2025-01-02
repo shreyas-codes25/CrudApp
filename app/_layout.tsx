eb72f8f65dcbe73d180dbeb959da3fc9c4b7c4fd
shreyas-codes25 <140911851+shreyas-codes25@users.noreply.github.com>
2025-01-02 22:48:51 +0530

    sad

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,13 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }}/>;
+  return (
+    <SafeAreaProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+      
+    </SafeAreaProvider>
+  );
 }
